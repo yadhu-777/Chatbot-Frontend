f33dee4beb0c954441b8d92c40c55b7068784212
yadhu krishna <yadhukris>
2026-03-08 17:41:41 +0530

    added auht

--- src/frontChat/College.jsx
+++ src/frontChat/College.jsx
@@ -15,7 +15,7 @@ export default function College(){
     return(
 
 <div className="outerCollege">
- { select == "Ai" ||  <OptionHead/>}
+  <OptionHead/>
 
 { select === "teacher" && <Teacher/>  }
 { select === "complain" && <Complain/>  }
