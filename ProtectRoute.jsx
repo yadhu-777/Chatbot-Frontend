1ef81cb32a397c99378458bf7cbe1ea095e4a939
yadhu krishna <yadhukris>
2026-03-10 21:47:19 +0530

    added auht

--- ProtectRoute.jsx
+++ ProtectRoute.jsx
@@ -1,46 +1,38 @@
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
-import { toast } from "react-toastify";
+import { Navigate, Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+function ProtectedRoute() {
 
+  const [auth, setAuth] = useState(null);
 
-function ProtectedRoute({ children }) {
-      const [auth, setAuth] = useState(null);
-     const navigate = useNavigate();
-async function checkAuth(){
-     fetch("https://chatbot-backend-0k0q.onrender.com/auth2", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-            },
+  useEffect(() => {
 
-          
-          })
-          .then((res)=>res.json())
-          .then((data)=>{
-            if (data.message === "not logged in") {
-            setAuth(false);
-        } else {
-            setAuth(true);
-        }
-           
-          })
-          if (auth === null) {
+    fetch("https://chatbot-backend-0k0q.onrender.com/auth2", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+
+      if (data.message === "not logged in") {
+        setAuth(false);
+      } else {
+        setAuth(true);
+      }
+
+    })
+    .catch(() => setAuth(false));
+
+  }, []);
+
+  if (auth === null) {
     return <div>Loading...</div>;
   }
 
   return auth ? <Outlet /> : <Navigate to="/admin" />;
-           
-}
-
-
-useEffect(() => {
-  checkAuth();
-}, []);
-
-  return <Outlet />;
 }
 
 export default ProtectedRoute;
