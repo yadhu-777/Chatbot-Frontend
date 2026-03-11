7c008c834ace8240e08fe2ab3c807e13db6c95e1
yadhu krishna <yadhukris>
2026-03-11 18:07:58 +0530

    added auht

--- src/frontChat/Form.jsx
+++ src/frontChat/Form.jsx
@@ -21,8 +21,8 @@ export default function Form() {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            email:email,
-            password:password,
+            email: email.trim(),
+            password,
           }),
         },
       );
