5444e99a24b95c1ad7e64bffcc775b05a5697322
yadhu krishna <yadhukris>
2026-03-11 18:13:14 +0530

    added auht

--- src/frontChat/Form.jsx
+++ src/frontChat/Form.jsx
@@ -1,98 +1,83 @@
 import ClearIcon from "@mui/icons-material/Clear";
 import { useContext, useState } from "react";
+import Mycontext from "../../Context";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import Mycontext from "../../Context";
+
 
 export default function Form() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  const { form, setForm } = useContext(Mycontext);
+  const [formVal,setFormVal] = useState(" ");
+   const [formVal2,setFormVal2] = useState(" ");
+  
+async function handleSubmit (){
+  const navigate =useNavigate();
+fetch ("https://chatbot-backend-0k0q.onrender.com/data",{
+  credentials: "include",
+  method:"POST",
+ 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        email:formVal,
+        password:formVal2
+      })
 
-  async function handleSubmit() {
-    try {
-      const res = await fetch(
-        "https://chatbot-backend-0k0q.onrender.com/data",
-        {
-          credentials: "include",
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password:password,
-          }),
-        },
-      );
-
-      const data = await res.json();
-
-      toast(data.message, {
-        position: "top-center",
+})
+.then((res)=>res.json())
+.then((data)=>
+  toast(data.message,{
+    position: "top-center",
         autoClose: 1000,
         theme: "dark",
-      });
-
-      if (res.ok) {
-        navigate("/clg");
-      }
-    } catch (err) {
-      toast("Login failed", {
-        position: "top-center",
-        autoClose: 1000,
-        theme: "dark",
-      });
-    }
   }
+    
+  ),
+  navigate("/clg")
+)
+}
+function handleChange(e){
+setFormVal(e.target.value)
+}
+function handleChange2(e){
+setFormVal2(e.target.value)
+}
 
+  const {form,setForm} = useContext(Mycontext)
   return (
     <div className="outerform">
       <div className="innerForm">
-        <div className="cross" onClick={() => setForm((prev) => !prev)}>
-          <ClearIcon className="clear" />
+        <div className="cross"  onClick={
+    ()=>setForm(prev=>!prev)
+}>
+      <ClearIcon  className="clear" ></ClearIcon  >
+
         </div>
+    
+     <form action="
+     ">
+   <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Event Name</label>
+  <input type="email" class="form-control" value={formVal} onChange={handleChange} id="exampleFormControlInput1" placeholder="name"/>
+</div>
+ <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Date </label>
+  <input type="email" class="form-control" value={formVal} onChange={handleChange} id="exampleFormControlInput1" placeholder="Date"/>
+</div>
 
-        <form>
-          <div className="mb-3">
-            <label htmlFor="emailInput" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              id="emailInput"
-              placeholder="you@example.com"
-            />
-          </div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Details of Event</label>
+  <textarea  value={formVal} onChange={handleChange2}  class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  <button style={{marginTop:"2rem"}} type="button" class="btn btn-secondary" onClick={handleSubmit} >Secondary</button>
+</div>
 
-          <div className="mb-3">
-            <label htmlFor="passwordInput" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              id="passwordInput"
-              placeholder="Password"
-            />
-          </div>
+     </form>
+       
 
-          <button
-            style={{ marginTop: "2rem" }}
-            type="button"
-            className="btn btn-secondary"
-            onClick={handleSubmit}
-          >
-            Login
-          </button>
-        </form>
+
+        
       </div>
     </div>
+
   );
 }
