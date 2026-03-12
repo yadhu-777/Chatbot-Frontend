2674f524c8249a50cdaf3f86050b1855c6125d76
yadhu krishna <yadhukris>
2026-03-12 16:25:58 +0530

    added auht

--- src/frontChat/Teacher.jsx
+++ src/frontChat/Teacher.jsx
@@ -62,7 +62,7 @@ useEffect(()=>{
 
           <CardContent sx={{ textAlign: "center", p: 3 }}>
             <Typography variant="h5" fontWeight="bold">
-            { teacherData[0][0].name}
+            { teacherData[0]?.name}
             </Typography>
 
             <Typography
@@ -84,11 +84,11 @@ useEffect(()=>{
                 },
               }}
             >
-             {teacherData[0].position}
+             {teacherData[0]?.position}
             </Typography>
 
             <Typography variant="body2" color="text.secondary">
-             {teacherData[0].details}
+             {teacherData[0]?.details}
             </Typography>
           </CardContent>
         </CardActionArea>
